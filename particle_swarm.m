b07204061ee73ede3515f56a5c13d293c4e569a7
VenkateshTK <venkatkrj@gmail.com>
2020-03-29 15:13:37 +0530

    single objective non constrained

--- particle_swarm.m
+++ particle_swarm.m
@@ -9,6 +9,7 @@ varmax = +5;
 maxit = 1000;
 npop =50;
 w=1;
+wdamp = 0.99;
 c1=2;
 c2=2;
 
@@ -36,6 +37,41 @@ for i = 1:npop
 end
 
 bestcost = zeros(maxit ,1);
+
+for it = 1:maxit
+   for i = 1:npop
+        particle(i).velocity = w*particle(i).velocity ...
+           + c1*rand(varsize).*(particle(i).best.position - particle(i).position) ...
+           + c2*rand(varsize).*(globalbest.position - particle(i).position);
+        particle(i).position =  particle(i).position + particle(i).velocity;
+        particle(i).cost = costfunction(particle(i).position);
+      
+        if particle(i).cost < particle(i).best.cost
+            particle(i).best.position = particle(i).position;
+            particle(i).best.cost = particle(i).cost;
+            % Update Global Best
+            if particle(i).best.cost < globalbest.cost
+                globalbest = particle(i).best;
+            end            
+        end
+   end
+        bestcost(it) = globalbest.cost;
+        
+        disp(['Iteration' num2str(it) ': best cost =' num2str(bestcost(it))]);
+        w = w*wdamp;
+        
+end
+
+figure;
+plot(bestcost,'LineWidth',2);
+xlabel('Iteration');
+ylabel('best cost');
+
+figure;
+semilogy(bestcost,'LineWidth',2);
+xlabel('Iteration');
+ylabel('best cost');
+grid on;
 
 
 function z= costfunction(x)
